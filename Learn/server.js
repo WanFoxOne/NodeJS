1eb38821299ab7ce0513d20b684ed443c5873ce7
WanFoxOne <corler@protonmail.com>
2017-10-05 11:29:59 +0200

    Initial commit

---
+++ Learn/server.js
@@ -0,0 +1,47 @@
+console.log("Initialisation ... | " + Date.now());
+
+// Librairies
+const express = require('express');
+const twig = require('twig');
+
+// Configuration
+const portDuServeur = 8080;
+
+// ---------------------------------------------------------------------
+
+let app = express();
+
+// Configuration de Twig
+app.set("twig options", {
+    strict_variables: false
+});
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Accueil');
+});
+
+app.get('/login', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Login');
+});
+
+app.get('/user/:username/test', function(req, res) {
+
+    if(!isNaN(req.params.username)) {
+        res.render('index.twig', {
+            message : req.params.username
+        });
+
+    } else {
+        res.status(404).send('Page introuvable !');
+    }
+
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(portDuServeur);
